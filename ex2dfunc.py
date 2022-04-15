e03aba999033ef829ad589287cfd02e3c96870c2
Shraddha <84399701+shr-code@users.noreply.github.com>
2022-04-15 12:40:09 +0530

    deleted unnecessary file

--- ex2dfunc.py
+++
@@ -1,7 +0,0 @@
-# changes made
-# some commit
-# git commit then write the commit statement and do git log
-# 4th commits for modification 
-#testing ki git commit work bhi krra ki nhi 5th commit
-print(huihuihui)
-# added to see checkout is safe in ex2 file
