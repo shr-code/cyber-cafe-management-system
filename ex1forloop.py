2901f60fd3e2192a23ba207eb897ae0b03d0bca8
Shraddha <84399701+shr-code@users.noreply.github.com>
2022-04-15 12:39:42 +0530

    deleted unnecessary file

--- ex1forloop.py
+++
@@ -1,4 +0,0 @@
-# this is just me practicing git and github
-# added to see checkout is safe in ex1 file
-#me checking the difference in working directory and staging area
-# mai abhi branch1 me change krri file ko
